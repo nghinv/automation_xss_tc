528b872b4a1dead5bf51b91e39681f4afd050342
Nghi Nguyen Van <nghinv@exoplatform.com>
2013-07-05 12:17:41 +0700

    TQA-1254: use COMM_assertAlertNotPresent

--- plf4.0.x/COMMON/automation_xss_take_screen_shot.sh
+++ plf4.0.x/COMMON/automation_xss_take_screen_shot.sh
@@ -6,7 +6,7 @@ interval=$3
   pushd ${output_prefix}_log
 counter=0
 #kill -9 `ps aux | grep shutter | awk '{print $2}'`
-sleep 10
+sleep 15
 while true; do
 
   if [ ! -d /proc/${parentpid} ]; then
@@ -17,8 +17,7 @@ while true; do
     echo "`date`, the report done, exit"
     exit 0
   fi
-  sleep 2
-#$((interval - 2))
+  sleep $((interval - 2))
   counter=$(( counter+1 ))
   #timeout ${interval} shutter -f -o ./screen_${counter}.jpg
   timeout $((interval*2)) import -window root ./screen_${counter}.jpg
